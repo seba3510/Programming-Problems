30578f06e6b85e5f908fa1f1185f4971cb0d5eba
seba3510 <seba3510@protonmail.com>
2023-07-13 15:53:05 -0400

    test: ddisplay array function tested

--- com/sebastian/arrays/ArrayUtils.java
+++ com/sebastian/arrays/ArrayUtils.java
@@ -1,8 +1,5 @@
 package com.sebastian.arrays;
 
-/**
- * ArrayUtils
- */
 public class ArrayUtils {
 
     public ArrayUtils() {
@@ -16,8 +13,7 @@ public class ArrayUtils {
      * @return
      *         {@link ArrayUtils#findMax(int[], int)}
      * 
-     * @throws EmptyArrayException
-     *                             If the given array is empty
+     * 
      */
     public int findMax(int[] arr) {
 
@@ -25,12 +21,19 @@ public class ArrayUtils {
 
         if ((n == 0)) {
 
-            return 0;
+            return 0; // array is empty
         }
 
         return findMax(arr, 0);
     }// findMax()
 
+    /**
+     * Helper method to find the maximum value in the array
+     * 
+     * @param arr
+     * @param i
+     * @return
+     */
     private int findMax(int[] arr, int i) {
 
         if ((i == arr.length - 1)) {
@@ -43,5 +46,55 @@ public class ArrayUtils {
         return max;
 
     }// findMax()
+
+    /**
+     * Reverses the order of the elements of the given array
+     * 
+     * @param arr
+     *            The array to reverse
+     * @return
+     *         A new array with the elements in reversed order
+     */
+    public int[] reverse(int[] arr) {
+
+        int n = arr.length;
+        int[] temp = new int[n];
+
+        int i = n - 1;
+
+        while ((i >= 0)) {
+
+            temp[i] = arr[i];
+            i--;
+        } // while loop
+        return temp;
+    }// reverse()
+
+    /**
+     * Returns a string representation of the given array
+     * 
+     * @param arr
+     *            The array to display
+     * @return
+     *         A string representation of the array
+     */
+    public String display(int[] arr) {
+
+        StringBuilder sb = new StringBuilder("[");
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+
+            if ((i != n - 1)) {
+
+                sb.append(", ");
+            }
+        } // for loop
+
+        return sb.append("]").toString();
+
+    }// display()
 
 }// class
